02fdc5c6971ebec48662a0cdfe07139c8624ade9
goncalhurtado <gonzalomilano@gmail.com>
2023-07-20 01:26:09 -0300

    termine el registro pero tengo inconvenientes con el login

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+//REGISTRARSE
 let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
 const formSignUp = document.querySelector("#registro")
+const formLogin = document.querySelector("#login")
+
 
 class Usuario {
-    constructor(id, nombre, email, password, isLogged = false) {
+    constructor(id, nombre, apellido, email, password, isLogged = false) {
         this.id = id;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.email = email;
         this.password = password;
         this.isLogged = isLogged;
@@ -26,6 +30,7 @@ formSignUp.addEventListener("submit", (e) => {
     e.preventDefault()
     let id = idRandom();
     let nombre = document.querySelector("#nombre-registro").value;
+    let apellido = document.querySelector("#apellido-registro").value;
     let email = document.querySelector("#email-registro").value;
     let password = document.querySelector("#password-registro").value;
     let error = document.querySelector(".error")
@@ -35,7 +40,7 @@ formSignUp.addEventListener("submit", (e) => {
     let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,6}$/;
     let emailValidado = regex.test(email); //esto devuelve true o false
 
-    if (nombre === "" || email === "" || password === "") {
+    if (nombre === "" || email === "" || apellido === "" || password === "") {
         error.innerHTML = `todos los campos son obligatorios`;
         error.style.display = "block"
         setTimeout(() => {
@@ -43,7 +48,7 @@ formSignUp.addEventListener("submit", (e) => {
         }, 4000)
         return;
     }
-    //Validar mail con expresion regular
+    //Validar mail con expresión regular
 
     if (!emailValidado) {
         error.innerHTML = `todos los campos son obligatorios`;
@@ -66,9 +71,43 @@ formSignUp.addEventListener("submit", (e) => {
         return;
     }
 
-    let nuevoUsuario = new Usuario(id, nombre, email, password)
+    let nuevoUsuario = new Usuario(id, nombre, apellido, email, password)
     usuarios.push(nuevoUsuario)
     localStorage.setItem("usuarios", JSON.stringify(usuarios))
     formSignUp.reset()
     alert(`usuario registrado correctamente`);
 })
+
+
+//INICIAR SESION
+
+// formLogin.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     let email = document.getElementById("#email-login").value;
+//     let password = document.getElementById("#password-login").value;
+//     const errorLogin = document.querySelector("error-login")
+//         //validar si existe el usuario con ese mail
+//     if (email === "" || password === "") {
+//         errorLogin.innerHTML = "todos los campos son obligatorios"
+//         errorLogin.style.display = "block"
+//         setTimeout(() => {
+//             error.style.display = "none"
+//         }, 4000)
+//         return;
+//     }
+//     let validarEmail = usuario.find((usuario) => {
+//         return usuario.email === email && usuario.password === password
+//     });
+
+//     if (validarEmail === undefined) {
+//         alert("usuario o contraseña son incorrectos")
+//         formSignUp.reset() //me limpia el formulario
+//         document.querySelector("#email-registro").focus()
+//         return;
+//     }
+//     validarEmail.isLogged = true;
+//     localStorage.setItem("usuarioLogueado", JSON.stringify(validarEmail))
+//     formLogin.reset();
+//     alert("usuario logueado con exito")
+//         // si existe el usuario y lo encuentra
+// })
