8ec532ff9c2fa759be1974b163acf96b756e3049
goncalhurtado <gonzalomilano@gmail.com>
2023-07-24 16:30:33 -0300

    agregue nuevas funcionalidades al reproductor

--- js/base-de-datos.js
+++ js/base-de-datos.js
@@ -1,31 +1,32 @@
 let canciones = JSON.parse(localStorage.getItem("canciones")) || [];
 
 class Cancion {
-    constructor(id, nombre, artista, mood, link, estaOculta = false) {
+    constructor(id, nombre, artista, mood, link, portada, estaOculta = false) {
         this.id = id;
         this.nombre = nombre;
         this.artista = artista;
         this.mood = mood;
         this.link = link;
+        this.portada = portada;
         this.estaOculta = estaOculta
 
     }
 }
 
 if (canciones.length === 0) {
-    let cancion1 = new Cancion(1, "Volveras a mi cama", "Fabian Show", "Alegre", "https://firebasestorage.googleapis.com/v0/b/pinkify-data.appspot.com/o/volverasAMiCama.mp3?alt=media&token=abd7809a-cc37-43ac-aa0f-55562c8b1d61")
+    let cancion1 = new Cancion(1, "Volveras a mi cama", "Fabian Show", "Alegre", "https://res.cloudinary.com/dhvgi2cmq/video/upload/v1690223762/pinkify/canciones/volverasAMiCama_fbglvl.mp3", "https://res.cloudinary.com/dhvgi2cmq/image/upload/v1690222404/pinkify/portada-volveras_axfvhk.jpg")
     canciones.push(cancion1)
     localStorage.setItem("canciones", JSON.stringify(canciones));
 
-    let cancion2 = new Cancion(2, "Persiana Americana", "Soda Stereo", "Alegre", "https://firebasestorage.googleapis.com/v0/b/pinkify-data.appspot.com/o/Persiana%20americana%3B%20Soda%20Stereo%20__Letra.mp3?alt=media&token=16dd656c-3acc-47ff-a6f5-89aca27c0460")
+    let cancion2 = new Cancion(2, "Persiana Americana", "Soda Stereo", "Alegre", "https://res.cloudinary.com/dhvgi2cmq/video/upload/v1690223772/pinkify/canciones/Persiana_americana_Soda_Stereo___Letra_xc84mt.mp3", "https://res.cloudinary.com/dhvgi2cmq/image/upload/v1690223347/pinkify/persiana_sbfenr.png")
     canciones.push(cancion2)
     localStorage.setItem("canciones", JSON.stringify(canciones));
 
-    let cancion3 = new Cancion(3, "Cancion para mi muerte", "Sui Generis", "Melancolico", "https://firebasestorage.googleapis.com/v0/b/pinkify-data.appspot.com/o/Persiana%20americana%3B%20Soda%20Stereo%20__Letra.mp3?alt=media&token=16dd656c-3acc-47ff-a6f5-89aca27c0460", true)
+    let cancion3 = new Cancion(3, "Cancion para mi muerte", "Sui Generis", "Melancolico", "https://res.cloudinary.com/dhvgi2cmq/video/upload/v1690223769/pinkify/canciones/Sui_Generis_-_Canci%C3%B3n_para_Mi_Muerte_Official_Audio_r1l966.webm", "https://res.cloudinary.com/dhvgi2cmq/image/upload/v1690223347/pinkify/portadas/cancionparamimuerte_hlb3jc.png")
     canciones.push(cancion3)
     localStorage.setItem("canciones", JSON.stringify(canciones));
 
-    let cancion4 = new Cancion(4, "De Música Ligera", "Soda Stereo", "Alegre", "https://firebasestorage.googleapis.com/v0/b/pinkify-data.appspot.com/o/Persiana%20americana%3B%20Soda%20Stereo%20__Letra.mp3?alt=media&token=16dd656c-3acc-47ff-a6f5-89aca27c0460")
+    let cancion4 = new Cancion(4, "De Música Ligera", "Soda Stereo", "Alegre", "https://res.cloudinary.com/dhvgi2cmq/video/upload/v1690223771/pinkify/canciones/Soda_Stereo_-_De_M%C3%BAsica_Ligera_nhthuv.mp3", "https://res.cloudinary.com/dhvgi2cmq/image/upload/v1690223347/pinkify/musicaligera_ybpt8w.png")
     canciones.push(cancion4)
     localStorage.setItem("canciones", JSON.stringify(canciones));
 
--- js/canciones.js
+++ js/canciones.js
@@ -31,18 +31,34 @@ function mostrarCanciones() {
     }
 
     // FUNCIONALIDAD BOTON PLAY
+
     cancionesSinOcultar.forEach(cancion => {
         const botonPlay = document.getElementById(`botonPlay-${cancion.id}`);
         botonPlay.addEventListener('click', (e) => {
             e.preventDefault();
+            let reproductor = document.getElementById(`reproductor`)
+            reproductor.classList.remove(`d-none`)
             const audioPlayer = document.getElementById('audioPlayer');
             const songUrl = cancion.link;
             audioPlayer.src = songUrl;
             audioPlayer.play();
             reproductorInfo.innerHTML = `${cancion.nombre} de ${cancion.artista}`
-
+            let portada = document.getElementById(`portada`);
+            portada.src = `${cancion.portada}`
         });
     });
+
+    //reproductor informacion
+    let contenedorPortada = document.getElementById(`contenedor-portada`)
+    let ocultarPortada = document.getElementById(`ocultar-portada`);
+    ocultarPortada.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        portada.classList.toggle(`d-none`)
+        ocultarPortada.classList.toggle(`position-absolute`)
+        contenedorPortada.classList.toggle(`position-relative`)
+        ocultarPortada.innerHTML = portada.classList.contains('d-none') ? `Mostrar Portada` : `Ocultar`
+    })
 
     function buscarCanciones() {
         const textoBuscado = document.getElementById('texto-buscador').value.toLowerCase();
@@ -77,11 +93,15 @@ function mostrarCanciones() {
             const botonPlay = document.getElementById(`botonPlay-${cancion.id}`);
             botonPlay.addEventListener('click', (e) => {
                 e.preventDefault();
+                let reproductor = document.getElementById(`reproductor`)
+                reproductor.classList.remove(`d-none`)
                 const audioPlayer = document.getElementById('audioPlayer');
                 const songUrl = cancion.link;
                 audioPlayer.src = songUrl;
                 audioPlayer.play();
-                reproductorInfo.innerHTML = `${cancion.nombre} de ${cancion.artista}`;
+                reproductorInfo.innerHTML = `${cancion.nombre} de ${cancion.artista}`
+                let portada = document.getElementById(`portada`);
+                portada.src = `${cancion.portada}`
             });
         });
     }
@@ -92,79 +112,6 @@ function mostrarCanciones() {
     });
 }
 
-
-// Función para realizar la búsqueda y mostrar solo las canciones que coincidan con el término buscado
-
-
-// Evento de clic para el botón de búsqueda
-
-
-// Evento de clic para el botón "Cancelar"
-// document.getElementById('botonCancelar').addEventListener('click', () => {
-//     mostrarTodasLasCanciones();
-// });
-
-
-
-
-
-// FUNCIONALIDAD BUSCADOR
-
-// let textoBuscador = document.getElementById(`texto-buscador`);
-// let botonBuscador = document.getElementById(`botonBuscador`);
-// let errorBuscador = document.getElementById(`error-buscador`);
-// let contenedorFiltro = document.getElementById(`contenedor-filtro`)
-// let textoFiltro = document.getElementById(`texto-filtro`)
-// let tablaCancionesFiltrada = document.getElementById(`tabla-canciones-filtrada`);
-// let quitarFiltro = document.getElementById(`quitar-filtro`);
-
-// botonBuscador.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const texto = textoBuscador.value;
-//     if (texto === ``) {
-//         errorBuscador.innerHTML = `Debe escribir algo`
-//         errorBuscador.style.display = "block"
-//         setTimeout(() => {
-//             errorBuscador.style.display = "none"
-//         }, 4000)
-//         return
-//     }
-//     data = JSON.parse(localStorage.getItem(`canciones`))
-//     let cancionesFiltradas = data.filter((cancion) => {
-//         return cancion.artista.includes(texto);
-//     })
-
-//     for (let i = 0; i < cancionesFiltradas.length; i++) {
-//         let cancion = cancionesFiltradas[i];
-
-//         //entiendo que acá esta el error porque lo estoy ocultando pero sigue estando completo
-//         tablaCanciones.style.display = "none"
-//         tablaCancionesFiltrada.innerHTML +=
-//             `
-//         <tr>
-//             <td>
-//                 <a id="botonPlay-${cancion.id}" href="" class="fa-solid fa-play" style="text-decoration: none;">
-//             </td>
-//             <th id="nombre-cancion" scope="row">${cancion.nombre}</th>
-//             <td id="artista-cancion">${cancion.artista}</td>
-//             <td>
-//                  <a href="" class="fa-sharp fa-regular fa-heart" style="text-decoration: none;">
-//             </td>
-//         </tr>
-//         `
-//     }
-//     console.log(`hay ${cancionesFiltradas.length} canciones filtradas`);
-
-
-//     //texto de filtro
-//     contenedorFiltro.classList.remove(`d-none`)
-//     textoFiltro.textContent += `${texto}`
-//     quitarFiltro.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         window.location.reload();
-//     })
-
-// })
 
 
 
